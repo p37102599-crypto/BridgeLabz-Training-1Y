b5a33cd65a2392674297414bbc7717a4d023ad58
Pragyan Gupta <p37102599gmail.com>
2026-01-13 14:47:05 +0530

    Added: AddTwoNumbers.java and AreaofCircle.java

---
+++ AreaofCircle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AreaofCircle{
+	public static void main(String[] args){
+		Scanner.sc = new Scanner(System.in);
+		double r = sc.nextDouble();
+		double ar = 3.14*r*r;
+		System.out.println("Area of Circle : " + ar);
+	}
+}
