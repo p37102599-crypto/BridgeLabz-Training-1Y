67a8f7d32be3fa053fd3194ea6db3efbb0cbee49
Pragyan Gupta <p37102599@gmail.com>
2026-01-13 22:11:49 +0530

    Added : Basic Programs for Programming Constructs

--- AreaofCircle.java
+++ AreaofCircle.java
@@ -4,6 +4,6 @@ public class AreaofCircle{
 		Scanner.sc = new Scanner(System.in);
 		double r = sc.nextDouble();
 		double ar = 3.14*r*r;
-		System.out.println("Area of Circle : " + ar);
+	System.out.println("Area of Circle : " + ar);
 	}
 }
