70221e301ce9c5e100ad4ed3e724b0f4ed7f3127
Pragyan Gupta <p37102599gmail.com>
2026-01-30 15:01:34 +0530

    Deleted

--- Basic-Programs/AreaofCircle.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class AreaofCircle{
-	public static void main(String[] args){
-		Scanner.sc = new Scanner(System.in);
-		double r = sc.nextDouble();
-		double ar = 3.14*r*r;
-	System.out.println("Area of Circle : " + ar);
-	}
-}
