4ad3d937942c4669d0eba5e670fdf870225e0fbb
Pragyan Gupta <p37102599gmail.com>
2026-02-02 14:25:22 +0530

    Added:ArrayExample.java

---
+++ Workshop/ArrayExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class ArrayExample {
+  public static void main() {
+     Scanner sc = new Scanner(System.in);
+     int marks[] = {10,20,30,40,50};
+System.out.println("Array Elements : ");
+      for(int i=0;i<marks.length;i++){
+      System .out.println(marks[i]);
+	 }
+  }
+ }
